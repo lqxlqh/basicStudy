aac9587ca1e48cf6abcbd2fb25c9b0fdaf790fa5
qhlin <1796362765@qq.com>
2019-12-03 18:37:33 +0800

    线程--创建线程

---
+++ Demo1/src/ThreadDemo/CreateThread/CreateThread1.java
@@ -0,0 +1,31 @@
+package ThreadDemo.CreateThread;
+
+/**
+ * @author lin
+ * @create 2019-12-03
+ * @Info(使用继承Thread类)
+ */
+class ThreadDemo extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getName()+":i="+i);
+        }
+    }
+}
+public class CreateThread1 {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadDemo threadDemo1 = new ThreadDemo();
+        threadDemo1.start();
+        for (int i = 0; i <100 ; i++) {
+            Thread.currentThread().sleep(100);
+            System.out.println(Thread.currentThread().getName()+":i="+i);
+        }
+
+    }
+}
